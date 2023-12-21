f4b24feb2df0818614f07e590bd5f2940f545d1f
Amin Mraidi <amine.mraidi@outlook.com>
2023-12-21 18:58:34 +0100

    removed unneeded comments

--- kartSimulator/evolutionary/simulation.py
+++ kartSimulator/evolutionary/simulation.py
@@ -1,21 +1,19 @@
-# Import the necessary libraries
 import torch
 import torch.nn as nn
 from pymoo.core.problem import Problem
 from pymoo.operators.crossover.pntx import SinglePointCrossover
 from pymoo.operators.mutation.bitflip import BitflipMutation
 
-# Define the number of bits for the integer and fractional parts of the weights
+import kartSimulator.evolutionary.utils as utils
+
 n_int = 4
 n_frac = 4
 
 
-# Define your neural network architecture
-# You may need to change this according to your problem definition
 class Network(nn.Module):
     def __init__(self):
+        # TODO shape similarly to PPO's NN
         super(Network, self).__init__()
-        # Define the layers and activation functions
         self.fc1 = nn.Linear(2, 4)
         self.relu1 = nn.ReLU()
         self.fc2 = nn.Linear(4, 2)
@@ -24,7 +22,6 @@ class Network(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        # Define the forward pass
         x = self.fc1(x)
         x = self.relu1(x)
         x = self.fc2(x)
@@ -33,15 +30,9 @@ class Network(nn.Module):
         x = self.sigmoid(x)
         return x
 
-# Define your problem class
-# You may need to change this according to your problem definition
 class SimRace(Problem):
     def __init__(self):
-        # Initialize the problem
-        # Define the number of variables, objectives, and constraints
-        # Define the lower and upper bounds of the variables
-        # Define the crossover and mutation operators
-        super().__init__(n_var=32, # 8 weights * 4 bits
+        super().__init__(n_var=32,
                          n_obj=2,
                          n_constr=0,
                          xl=0,
@@ -51,42 +42,30 @@ class SimRace(Problem):
                          crossover=SinglePointCrossover(),
                          mutation=BitflipMutation())
 
-        utils
-        # Create an instance of the network
         self.network = Network()
 
     def _evaluate(self, x, out, *args, **kwargs):
-        # Evaluate a single solution (chromosome)
-        # Convert the chromosome to a tensor of weights
-        #print(x[0])
+        # FIXME does not generate binary specimen
         weights = []
         for i in range(8):
-            # Extract the binary string for each weight
             bin_str = "".join(map(str, x[i*4:(i+1)*4]))
-            #print(bin_str)
-            # Convert the binary string to a decimal number
             dec_num = utils.bin2dec(bin_str, n_int, n_frac)
-            # Append the decimal number to the weights list
             weights.append(dec_num)
-        # Create a tensor from the weights list
-        tensor = torch.tensor(weights)
-        # Load the tensor into the network
-        self.network.load_state_dict(tensor)
-        # Save the network state
 
+        tensor = torch.tensor(weights)
+        self.network.load_state_dict(tensor)
+        # FIXME
         #torch.save(self.network.state_dict(), 'network.pth')
 
-        # Define the input variables for the network
-        # You may need to change this according to your problem definition
         x1 = 0.5
         x2 = 0.75
-        # Create a tensor from the input variables
-        input = torch.tensor([x1, x2])
-        # Make a prediction using the network
-        output = self.network(input)
-        # Define the objective functions
-        # You may need to change this according to your problem definition
+
+        input_tensor = torch.tensor([x1, x2])
+
+        output = self.network(input_tensor)
+
+        # TODO define the problem properly
         f1 = output
         f2 = 1 - output
-        # Assign the objective values to the output dictionary
+
         out["F"] = [f1, f2]
